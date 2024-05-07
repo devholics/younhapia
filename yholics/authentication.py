67b41e45c21b71a6bb3144004aae956bfe981498
Riever <riever.holic@gmail.com>
2024-05-07 16:13:24 -0500

    Fix UserSessionAuthentication

--- yholics/authentication.py
+++ yholics/authentication.py
@@ -1,17 +1,11 @@
 from rest_framework import authentication
 
-from allauth.usersessions.models import UserSession
-
 
 class UserSessionAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         session_token = request.headers.get("x-session-token")
-        if session_token:
-            try:
-                session = UserSession.objects.get(session_key=session_token)
-                if not session.purge():
-                    user = session.user
-                    if user and user.is_active:
-                        return user, None
-            except UserSession.DoesNotExist:
-                return None
+        # CSRF attack prevention
+        if session_token == request.session.session_key:
+            user = request.user
+            if user.is_authenticated and user.is_active:
+                return user, None
